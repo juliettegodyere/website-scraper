0cf5e41e775f1b5df4b70b8a812c5ce4afe413e3
Juliet <juliet.nkwor@pmglobaltechnology.com>
2017-11-08 15:51:12 +0100

    Added policy number url

--- index.js
+++ index.js
@@ -79,13 +79,86 @@ function Scraper(serviceId, conf){
         })
     }
 }
+function PolicyNumber(serviceId, conf){
+    var configuration = conf;
+    var scrapeData = [];
+    var policyPage = function(url, policy_number){
+        return new Promise(function(resolve, reject){
+            console.log('Evaluating '+url);
+            var request = require("request");
 
+            var options = { method: 'POST',
+                url: 'http://www.askniid.org/checkpolicy.aspx',
+                headers:
+                    { 'postman-token': 'edf3f125-1835-3958-bb76-4c73627c2a52',
+                        referer: 'http://www.askniid.org/checkpolicy.aspx',
+                        'x-microsoftajax': 'Delta=true',
+                        connection: 'keep-alive',
+                        'x-requested-with': 'XMLHttpRequest',
+                        'cache-control': 'no-cache',
+                        accept: '*/*',
+                        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+                        'accept-language': 'en-CA,en-GB;q=0.8,en-US;q=0.6,en;q=0.4',
+                        'accept-encoding': 'gzip, deflate',
+                        origin: 'http://www.askniid.org',
+                        cookie: '_ga=GA1.2.193461287.1509717203' },
+                body: 'ctl00%24ScriptManager1=ctl00%24ContentPlaceHolder1%24UpdatePanel1%7Cctl00%24ContentPlaceHolder1%24btnSearch&ctl00%24ContentPlaceHolder1%24drpOption=Single&ctl00%24ContentPlaceHolder1%24rblNumber=Policy%20Number&ctl00%24ContentPlaceHolder1%24txtNumber='+policy_number+'&__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&__VIEWSTATE=VFSgutVJcf03FkuTy5iaPuNMKmgJq28ZMgBX8VCqmV%2Fqq2hCWQXzpEl9BxdBY9nSEIu0EXnSEayLpJgXjqXfD4E9jXAYWSdU3IGQMUhzt61wKExxGTPx1Ze9oGN8%2BEMJhWbIDbqZH1beXprqOSzWZo5dG%2F1%2FmKPvodIGPZgxCVZ9bwvSO3Sbh%2FEKtsAJISerYL%2FFZklKeDboEPOdrWqNPSK2XdsiJtSJQGIh1CrDB2iHUz%2Bz3r3M8NflY6%2BIRELtLOSSpUIYFCXao8XeSH0A%2BPJ1l1D0fNfOuoqki9IWhiKPte9VqEgB5pHP1Fz%2FQO%2FYU0irBEUsEnPTO6O2aDxuVNaQm6MD6bt4vbh6HMNp55G4DxJ2zytJtKJjxO0nsV8xu5kyopXyWJuxXE2LkAxTpU4AbHcoylqWfQZGJapz7nRVXVC93t3U0R1uPKR3xBtml20VvBMky%2BKKJmRsPhItvHGY7AjwF35l5txgEcRJKxZilwy%2BHw%2Bnriqfq%2BLe%2Fd7KWyxJ1OBTumUkn3dGQv0xOFM8c5hxr3tlMbt0X47H2m3CgGy%2BcF13RY%2FOgx2AJZ9JtJHgxNzoACfEsts6%2Bn114qiTcXi90qYf85NhyyD15gYFXNo7pDBAOolaz1FUvmHwRRdTQnEcrVJTXdBL06Yd6DRUOR2DdlFimtVmJwW5lwHisQKPg4IlKnRyfoGnxcar0gehQEY3zTPdBXSdQPb1Gy%2FAq7vzNk6CvCNyEJ%2BxQCz5GNsM6oXzWsvEQolxHgn2y62Su4JwBpfluCkngCRsZzPJNEE1vP9rVE2wO3ZvLD99GWAvKDQ9Z1ZukQZwfMvhFnTVat7zQBHNOVMyH1xgHFMYWDxH1aigvNRj1SVJYJFVQV%2BZcGHlQdqHbi7CsB0fF3YW1PfYD5fI6ojGjIvhq1vr0zhXbOzae%2FdBYZRqiQg%2FWrZafyc7SkU1rgqDFmr6lIvQzNHwfpTu2Yor9heh%2F6cbNL2PW3AbahDPbwCftpULvcMusVN%2FeorEkpqIk7vz9wo9V4XAeUUJ9ASfP7HEQ8Hwe7%2FQ6Lddp3avvu8NKUhL6V9QNDjYcXZ%2BOqb96uomkR6cyyyaOJF99rsE1gmme2IfBHphKvNCS92dZDWeDgvGjcEsLd5sahajoxQSw1yaCi%2F72ds2HQ1nI6Nz5X6n2EybZI9pkkqsIqZoaha8159hrWJCq0cTL1nOffP%2Fmph5s2SlR%2FpnIKrZEmo%2FCh7eB%2B2rZpAFq%2FGJYMpQCOOwyWlDhGvcv4WuFpGIleArNqWvdA9N%2FP2YIcR4c252361iuBpoebdyx0gg3vQWj%2BWivsPpvAvQW%2BuO9cV11DExEjDithlNJaf5cvDVdbso3LnENjlNX3g%2BcN8ILjl3NslsVO9uZPV6KaXf31vsumpSAB5ZyNQW7VCiCWP6IzoEQ85W5cU8gCKCGY7ZVhg4yUB1gRA%3D&__VIEWSTATEGENERATOR=FD5FD3B2&__VIEWSTATEENCRYPTED=&__ASYNCPOST=true&ctl00%24ContentPlaceHolder1%24btnSearch=Search' };
+
+            request(options, function (error, response, body) {
+                if(!error){
+                    resolve(body);
+                } else {
+                    reject(error);
+                }
+            });
+        });
+    };
+    return {
+        start : async(function(req, res){
+            if (!req.query.policy_number) {
+                res.status(400);
+                return res.send({
+                    status : 'failed',
+                    result : 'We need a Pin query parameter'
+                });
+            }
+
+            var url = "http://www.askniid.org/CheckPolicy.aspx";
+            var html = await(policyPage(url, req.query.policy_number));
+            //console.log(html);
+            var $ = cheerio.load(html);
+            var container = $("#ContentPlaceHolder1_TB_SinglePolicy tr");
+            if (!container.length) {
+                res.status(404);
+                return res.send({
+                    status : 'failed',
+                    result : 'No Policy Found'
+                });
+            }
+            var resp = {};
+            for (var i = 1; i < container.length; i++){
+                var key = $("#ContentPlaceHolder1_TB_SinglePolicy tr:nth-of-type("+i+") .chill").html();
+                var val = $("#ContentPlaceHolder1_TB_SinglePolicy tr:nth-of-type("+i+") span").html();
+                resp[key] = val;
+            }
+            console.log(resp);
+            res.status(200);
+            return res.send({
+                status : 'success',
+                result : resp
+            });
+        })
+    }
+}
 var url = process.env.PORT || 2000;
 
 app.get('/', function(req, res){
     new Scraper(1, 1).start(req, res);
 });
-
+app.get('/policy', function(req, res){
+    new PolicyNumber(1, 1).start(req, res);
+});
 app.listen(url, function(){
     console.log('Listening on '+url);
 });
